d95fec8c8cb2b1ab9b507f226bf8df0a486a5c1b
ziad elsharkawy <ziadessamcr7@gmail.com>
2024-06-03 10:29:25 +0300

    edit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 var searchInput = document.getElementById('searchInput')
 
-console.log ( searchInput.value )
 //today variables
 var today_name = document.getElementById('today_name');
 var today_number = document.getElementById('today_number');
@@ -8,7 +7,7 @@ var today_month = document.getElementById('today_month');
 var city_name = document.getElementById('city_name')
 var temp_in_C = document.getElementById('temp_in_C')
 var today_condition_img = document.getElementById('today_condition_img')
-console.log( today_condition_img )
+console.log(today_condition_img)
 var today_condition = document.getElementById('today_condition')
 var humidity = document.getElementById('humidity')
 var wind_velocity = document.getElementById('wind_velocity')
@@ -21,64 +20,60 @@ var max_temp = document.getElementsByClassName('max_temp')
 var min_temp = document.getElementsByClassName('min_temp')
 var condition = document.getElementsByClassName('condition')
 
-console.log( condition)
+
+var weatherArray = []
 
 
-
-
-var weatherArray = [] 
-
-
-function searchData(term="paris"){
+function searchData(term = "cairo") {
     var myHttpReq = new XMLHttpRequest();
 
-myHttpReq.open('GET' , `https://api.weatherapi.com/v1/forecast.json?key=788e204a8a844367883225727232708&q=${term}&days=3`)
+    myHttpReq.open('GET', `https://api.weatherapi.com/v1/forecast.json?key=788e204a8a844367883225727232708&q=${term}&days=3`)
 
-myHttpReq.send();
+    myHttpReq.send();
 
-myHttpReq.addEventListener('loadend' , function(){
-    if ( myHttpReq.status == 200 ) {
-        weatherArray = JSON.parse( myHttpReq.response )
-        console.log( weatherArray)    
-        DisplayTodayWeather()
-        displayNextTwoDays()
+    myHttpReq.addEventListener('loadend', function () {
+        if (myHttpReq.status == 200) {
+            weatherArray = JSON.parse(myHttpReq.response)
+            console.log(weatherArray)
+            DisplayTodayWeather()
+            displayNextTwoDays()
 
-    }
-})
+        }
+    })
 }
+searchData()
 
 
 
-
-function DisplayTodayWeather(){
+function DisplayTodayWeather() {
     var todayDate = new Date()
 
 
-    today_name.innerHTML = todayDate.toLocaleDateString('en-us',{weekday:'long'})
+    today_name.innerHTML = todayDate.toLocaleDateString('en-us', { weekday: 'long' })
     today_number.innerHTML = todayDate.getDate()
-    today_month.innerHTML = todayDate.toLocaleDateString('en-us',{month:'long'})
+    today_month.innerHTML = todayDate.toLocaleDateString('en-us', { month: 'long' })
     city_name.innerHTML = weatherArray.location.name
     temp_in_C.innerHTML = weatherArray.current.temp_c + '<sup>o</sup>C'
     today_condition_img.src = weatherArray.current.condition.icon
     today_condition.innerHTML = weatherArray.current.condition.text
     humidity.innerHTML = weatherArray.current.humidity + '%'
-    wind_velocity.innerHTML = weatherArray.current.wind_kph +'km/h'
+    wind_velocity.innerHTML = weatherArray.current.wind_kph + 'km/h'
     wind_dir.innerHTML = weatherArray.current.wind_dir
 
 }
 
-function displayNextTwoDays(){
+function displayNextTwoDays() {
 
-    for( var i = 0; i < 2; i++ ){
-        
-        var nextDay = new Date(weatherArray.forecast.forecastday[i+1].date)
+    for (var i = 0; i < 2; i++) {
 
-        nameOfNextDay[i].innerHTML = nextDay.toLocaleDateString('en-us',{weekday:'long'})
+        var nextDay = new Date(weatherArray.forecast.forecastday[i + 1].date)
 
-        max_temp[i].innerHTML =   weatherArray.forecast.forecastday[i+1].day.maxtemp_c +'<sup>o</sup>C'
-        min_temp[i].innerHTML =   weatherArray.forecast.forecastday[i+1].day.mintemp_c + '<sup>o</sup>C'
-        condition_img[i].src =   weatherArray.forecast.forecastday[i+1].day.condition.icon
-        condition[i].innerHTML = weatherArray.forecast.forecastday[i+1].day.condition.text
+        nameOfNextDay[i].innerHTML = nextDay.toLocaleDateString('en-us', { weekday: 'long' })
+
+        max_temp[i].innerHTML = weatherArray.forecast.forecastday[i + 1].day.maxtemp_c + '<sup>o</sup>C'
+        min_temp[i].innerHTML = weatherArray.forecast.forecastday[i + 1].day.mintemp_c + '<sup>o</sup>C'
+        condition_img[i].src = weatherArray.forecast.forecastday[i + 1].day.condition.icon
+        condition[i].innerHTML = weatherArray.forecast.forecastday[i + 1].day.condition.text
     }
 
 }
